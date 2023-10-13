7c3538b03f9abc18bf4588c0dffd05bd61661caa
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-13 11:23:13 +0000

    Added create table tool into create_tables.sql. Added script schema to package.json.

--- src/models/create_tables.sql
+++ src/models/create_tables.sql
@@ -3,7 +3,14 @@ CREATE TABLE tool(
   tool_id numeric PRIMARY KEY,
   name text,
   author_company text,
-  type text
+  type text,
+  specialization text,
+  license text,
+  isOpenSource boolean,
+  isPeriodicPay boolean,
+  downloads numeric,
+  searches numeric,
+  easeOfLearning numeric
 );
 
 
