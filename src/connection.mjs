1f141d77b92ed5e64ddef460ebb224db7c311daf
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-13 11:15:59 +0000

    Able to insert rows into table.

--- src/connection.mjs
+++ src/connection.mjs
@@ -1,10 +1,19 @@
 import pgPromise from 'pg-promise';
 const pgp = pgPromise();
-const db = pgp('postgres://postgres:foo@localhost:5432/raw_database');
+const db = pgp({
+  connectionString: 'postgres://postgres:foo@localhost:5432/raw_database',
+  max: 20
+});
 
-db.query('SELECT * FROM tool')
+db
+  .none('DELETE FROM tool WHERE tool_id = 1')
+  .then(() => {
+    return db.none(`INSERT INTO tool(tool_id, name, author_company, type) VALUES
+    (1, 'ansible', 'Michael Dehaan', 'Automation')`);
+  })
+  .then(() => db.query('SELECT * FROM tool'))
   .then(data => {
-    console.log('datos: ' + data);
+    console.log('datos: ' + JSON.stringify(data, null, 2));
   })
   .catch(error => {
     console.error('Error:', error);
