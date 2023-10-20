1f0b8c43987acc2c147adc601ffd079026bd22a4
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-20 11:02:15 +0000

    Parsed the name and author of each item in github explore frontend topic.

--- src/routes/scrap-frontend.mjs
+++ src/routes/scrap-frontend.mjs
@@ -12,12 +12,21 @@
 
 import { CheerioCrawler, purgeDefaultStorages } from "crawlee";
 
+
+
 const crawler = new CheerioCrawler({
   async requestHandler({ $, request, enqueueLinks }) {
     await purgeDefaultStorages();
     const topicTitle = $('.gutter-md .d-flex.flex-1 .h1').text().trim();
     console.log('Title: ' + topicTitle);
-    
+    const allNameAndAuthor = $('article div.d-flex.flex-1 h3')
+        .map((_, element) => {
+          const children = $(element).children();
+          const name = $(children.get(0)).text().trim();
+          const author = $(children.get(1)).text().trim();
+          return { name, author }
+        })
+        .toArray().forEach((e) => console.log(`${e.name}/${e.author}`));
   }
 });
 
