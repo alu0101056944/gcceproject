6383e440698bdb63c4f91d12d680d7ac30030f8c
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-22 15:01:02 +0000

    Used infiniteScrolling function instead on the github topic explore scrap script.

--- src/routes/scrap-github-explore.mjs
+++ src/routes/scrap-github-explore.mjs
@@ -5,16 +5,17 @@
  *
  * @see {@link https://github.com/topics/frontend} for the webpage it was tested
  *  on.
+ * @see {@link https://blog.apify.com/how-to-scrape-the-web-with-playwright-ece1ced75f73/}
+ *    for a github explore playwright scrap example.
  */
 
 'use strict';
 
-import { PlaywrightCrawler, purgeDefaultStorages, Configuration } from 'crawlee';
+import { PlaywrightCrawler, purgeDefaultStorages } from 'crawlee';
 
 const crawler = new PlaywrightCrawler({
-  headless: false,
-  navigationTimeoutSecs: 600,
-  async requestHandler({ page, request, enqueueLinks }) {
+  headless: true,
+  async requestHandler({ page, request, enqueueLinks, infiniteScroll }) {
     await purgeDefaultStorages();
     const topicTitleContainer =
         await page.waitForSelector('.gutter-md .d-flex.flex-1 .h1')
@@ -22,13 +23,15 @@ const crawler = new PlaywrightCrawler({
     const topicTitle = topicTitleHandler.trim();
     console.log('Title: ' + topicTitle);
     const AMOUNT_OF_RESULTS = await extractAmountOfResults(page);
-    console.log(AMOUNT_OF_RESULTS);
+    console.log('Amount of results: ' + AMOUNT_OF_RESULTS);
+    await infiniteScroll({
+      buttonSelector: 'text=Load more',
+      stopScrollCallback: async () => {
+        const repos = await page.$$('article.border');
+        return repos.length >= AMOUNT_OF_RESULTS;
+      }
+    })
     const mapNameAndAuthor = await makeArrayOfUsefulInfo(page);
-    const INITIAL_AMOUNT_OF_RESULTS = mapNameAndAuthor.length;
-    const moreButton = page.getByRole('button', { name: /Load.*more/i });
-    for (let i = 0; i < 40; i++) {
-      await moreButton.click();
-    }
     mapNameAndAuthor.forEach((e) => console.log(`${e.name}/${e.author}`));
   },
 });
