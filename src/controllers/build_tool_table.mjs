d15d9c26c6ca78d033ecd4d710cd506287e7bb5f
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-10-22 21:30:16 +0100

    Build tool table file added to controllers.

---
+++ src/controllers/build_tool_table.mjs
@@ -0,0 +1,30 @@
+/**
+ * @author Marcos Barrios
+ * @since 22_10_2023
+ * @desc Build a tool table
+ */
+
+'use strict';
+
+const types = [
+  'language',
+  'testing',
+  'db',
+  'editor',
+  ['machine-learning', 'deep-learning', 'deep-neural-networks'],
+  'code-quality',
+  'code-review',
+  'compiler',
+  'continuous-integration',
+  'devops',
+  'documentation',
+];
+
+const specializations = [
+  'front-end',
+  'back-end',
+  'embedded',
+  'devops',
+];
+
+
