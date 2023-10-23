d8389409c143941231a79bb6396f4e9ce857ff45
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-10-23 14:22:49 +0100

    Added artificial fill values into date, community, company, project, market tables.

--- src/models/fill_artificial_raw.sql
+++ src/models/fill_artificial_raw.sql
@@ -22,31 +22,21 @@ INSERT INTO employee(employee_id, name, title, department)
     (19, 'Linda Lewis', 'Software Engineer', 'IT'),
     (20, 'Thomas Moore', 'Software Engineer', 'IT');
 
-INSERT INTO company(company_id, name, employee_amount, amount_of_searches)
-  VALUES (1, 'gcce', 20, 28732);
-
-
 INSERT INTO tool (
     tool_id,
     name,
     author_company,
     type,
     specialization,
-    license,
-    isOpenSource,
-    isPeriodicPay,
-    downloads,
-    searches,
-    easeOfLearning
 ) VALUES
-    (1, 'JavaScript', 'Various', 'Programming Language', 'Web Development', 'MIT', true, false, 1000000, 50000, 4),
-    (2, 'Ansible', 'Red Hat', 'Automation Tool', 'Infrastructure Automation', 'GPLv3', true, false, 150000, 10000, 3),
-    (3, 'Git', 'Various', 'Version Control System', 'Software Development', 'GPL', true, false, 5000000, 200000, 5),
-    (4, 'GitHub', 'GitHub, Inc.', 'Version Control System', 'Software Development', 'Proprietary', true, true, 20000000, 1000000, 3),
-    (5, 'Sass', 'Various', 'CSS Preprocessor', 'Web Development', 'MIT', true, false, 50000, 5000, 3),
-    (6, 'Ruby', 'Various', 'Programming Language', 'Web Development', 'Ruby License', true, false, 100000, 10000, 2),
-    (7, 'React.js', 'Facebook', 'JavaScript Library', 'Front-end Development', 'MIT', true, false, 5000000, 300000, 4),
-    (8, 'PostgreSQL', 'PostgreSQL Global Development Group', 'Database Management System', 'Database', 'PostgreSQL License', true, false, 2000000, 100000, 4);
+    (1, 'JavaScript', 'Various', 'Programming Language', 'Web Development'),
+    (2, 'Ansible', 'Red Hat', 'Automation Tool', 'Infrastructure Automation'),
+    (3, 'Git', 'Various', 'Version Control System', 'Software Development'),
+    (4, 'GitHub', 'GitHub, Inc.', 'Version Control System', 'Software Development'),
+    (5, 'Sass', 'Various', 'CSS Preprocessor', 'Web Development'),
+    (6, 'Ruby', 'Various', 'Programming Language', 'Web Development'),
+    (7, 'React.js', 'Facebook', 'JavaScript Library', 'Front-end Development'),
+    (8, 'PostgreSQL', 'PostgreSQL Global Development Group', 'Database Management System', 'Database');
 
 
 INSERT INTO employee_tool(employee_id, tool_id, years_of_experience)
@@ -211,4 +201,41 @@ INSERT INTO employee_tool(employee_id, tool_id, years_of_experience)
     (20, 6, 5),
     (20, 7, 5),
     (20, 8, 4);
+
+INSERT INTO date(date_id, date)
+  VALUES
+    (1, '2023-10-21'),
+    (2, '2023-10-22'),
+    (3, '2023-10-23');
+
+INSERT INTO community(community_id, name, type)
+  VALUES
+    (1, 'github', 'hosting');
+    (2, 'stackoverflow', 'forum');
+
+INSERT INTO company(company_id, name, employee_amount, amount_of_searches, type)
+  VALUES
+    (1, 'Various', 5000, 1000000, 'Diverse Company'),
+    (2, 'Red Hat', 12000, 2500000, 'Software Company'),
+    (3, 'GitHub, Inc.', 1500, 300000, 'Software Development Company'),
+    (4, 'Facebook', 40000, 5000000, 'Social Media Company'),
+    (5, 'PostgreSQL Global Development Group', 50, 50000, 'Open Source Organization');
+
+INSERT INTO project (project_id, project_name, status, downloads, contributors, searches)
+  VALUES
+    (1, 'JavaScript', 'Active', 10000000, 5000, 2000000),
+    (2, 'Ansible', 'Active', 500000, 2000, 100000),
+    (3, 'Git', 'Active', 80000000, 3000, 3000000),
+    (4, 'GitHub', 'Active', 150000000, 10000, 5000000),
+    (5, 'Sass', 'Active', 3000000, 1000, 200000),
+    (6, 'Ruby', 'Active', 700000, 300, 10000),
+    (7, 'React.js', 'Active', 2500000, 5000, 800000),
+    (8, 'PostgreSQL', 'Active', 5000000, 2000, 300000);
+
+INSERT INTO market (market_id)
+  VALUES
+    (1),
+    (2),
+    (3);
+
 
