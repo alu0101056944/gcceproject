027d8fdde473f412f0db33ef94f5e6692ef8211f
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-10-28 20:33:44 +0100

    Able to use NamesTuURLScrapper, tested.

--- src/controllers/add_downloads_to_table.mjs
+++ src/controllers/add_downloads_to_table.mjs
@@ -8,14 +8,33 @@
 
 import { inspect } from 'util';
 import NPMJSScrapper from '../routes/npmjs-scrapper.mjs';
+import NamesToURLScrapper from '../routes/names-to-urls-scrapper.mjs';
 
 export default function addDownloads() {
   const packageNames = [
     'react',
     'eslint-plugin-react',
   ];
-  const scrapper = new NPMJSScrapper(packageNames);
-  scrapper.run().then(() => console.log(inspect(scrapper.getOutputObject())));
+
+  // If a package is present on more than one programming language, then
+  // get the one with the most downloads
+  // for example:
+  //  react is in npmjs and pypi, but pypi's is an impersonification.
+
+  // const scrapperNPM = new NPMJSScrapper(packageNames);
+  // scrapperNPM.run().then(() => console.log(inspect(scrapperNPM.getOutputObject())));
+
+  const URL_PREFIX = 'https://pypistats.org/packages/';
+  const URL_POSTFIX = '';
+  const scrapperPython = new NamesToURLScrapper(
+        packageNames,
+        URL_PREFIX,
+        URL_POSTFIX,
+        ({ page, request, log }) => {
+          log.info('PythonScrapper visited ' + request.url);
+        },
+      );
+  scrapperPython.run().then(() => console.log('Python scrapper finished'));
 }
 
 addDownloads();
