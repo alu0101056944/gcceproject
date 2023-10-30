c3cfc76c2f1fd7e780f554a4f512cd26afde142b
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-10-30 09:36:39 +0000

    Scrapped commit amount from repository.

---
+++ src/controllers/get_repository_info.mjs
@@ -0,0 +1,47 @@
+/**
+ * @author Marcos Barrios
+ * @since 30_10_2023
+ */
+
+'use strict';
+
+import { inspect } from 'util';
+import NamesToURLScrapper from '../routes/names-to-urls-scrapper.mjs';
+import { expect } from 'playwright/test'
+
+export default async function addInfo() {
+  const allRepositories = [
+    'facebook/react',
+    'teidesat/SpaceRad-memory-test',
+  ];
+
+  const URL_PREFIX = 'https://github.com/';
+  const URL_POSTFIX = '';
+  const scrapperRepositoryInfo = new NamesToURLScrapper(
+        allRepositories,
+        URL_PREFIX,
+        URL_POSTFIX,
+        async ({ page, request, log, outputObject }) => {
+          log.info('GithubInfoScrapper visited ' + request.url);
+          const commitsLocator = page.locator('span.d-none.d-sm-inline')
+              page.locator('strong');
+          const allcommitsLocator = await commitsLocator.all();
+
+          const AMOUNT_OF_COMMITS_STRING =
+              await allcommitsLocator[1].textContent();
+          const AMOUNT_OF_COMMITS_PROCESSED = AMOUNT_OF_COMMITS_STRING.trim()
+              .replace(/,/g, '');
+          const AMOUNT_OF_COMMITS = parseInt(AMOUNT_OF_COMMITS_PROCESSED);
+
+          if (!outputObject[request.label]) {
+            outputObject[request.label] = {}
+          } 
+          outputObject[request.label].commits = AMOUNT_OF_COMMITS;
+        },
+      );
+  scrapperRepositoryInfo.create();
+  const output = await scrapperRepositoryInfo.run();
+  console.log(inspect(output));
+}
+
+addInfo();
