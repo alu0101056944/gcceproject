fc1728c6e8ebc434752876febf0f00e6fb3e7645
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-10-30 13:46:44 +0000

    Added make date registry file.

---
+++ src/controllers/add_date_entry_to_date_table.mjs
@@ -0,0 +1,31 @@
+/**
+ * @author Marcos Barrios
+ * @since 30_10_2023
+ * @description Make date table.
+ * 
+ */
+
+'use strict';
+
+import { readFile, writeFile } from 'fs/promises'
+
+
+import { inspect } from 'util';
+
+export default async function addDateEntry() {
+  const FILE_CONTENT = await readFile('./src/persistent_ids.json', 'utf8');
+  const persistentIds = JSON.parse(FILE_CONTENT);
+
+  ++persistentIds.date;
+  const dateEntry = {
+    date_id: persistentIds.date,
+    date: new Date(),
+  };
+
+  const TO_JSON = JSON.stringify(persistentIds, null, 2);
+  await writeFile('./src/persistent_ids.json', TO_JSON);
+
+  return dateEntry;
+}
+
+console.log((await addDateEntry()));
