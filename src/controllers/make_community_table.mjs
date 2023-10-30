7577e822e33a4fa3746bcf36a39eb9604e6b8d1e
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-10-30 13:51:45 +0000

    Made make community table file.

---
+++ src/controllers/make_community_table.mjs
@@ -0,0 +1,30 @@
+/**
+ * @author Marcos Barrios
+ * @since 30_10_2023
+ * @description Make community table.
+ * 
+ */
+
+'use strict';
+
+import { readFile, writeFile } from 'fs/promises'
+
+export default async function makeTable() {
+  const records = [
+    {
+      community_id: 1,
+      name: 'github',
+      type: 'Open Source Hosting',
+    },
+  ];
+
+  const FILE_CONTENT = await readFile('./src/persistent_ids.json', 'utf8');
+  const persistentIds = JSON.parse(FILE_CONTENT);
+  persistentIds.community = 1;
+  const TO_JSON = JSON.stringify(persistentIds, null, 2);
+  await writeFile('./src/persistent_ids.json', TO_JSON);
+
+  return records;
+}
+
+// makeTable().then((data) => console.log(data));
