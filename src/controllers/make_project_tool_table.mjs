6a2c7d9684ae7306057230d4c4f5d2fbc5736791
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-11-03 13:31:16 +0000

    Somewhat finished make project_tool table.

--- src/controllers/make_project_tool_table.mjs
+++ src/controllers/make_project_tool_table.mjs
@@ -13,19 +13,38 @@ import GithubDependenciesScrapper from "../routes/github-dependencies-scrapper.m
 // import { inspect } from 'util';
 
 /**
- * @param {object} toolTable ready for database insertion
- * @param {object} projectTable ready for database insertion
+ * @param {array} toolTable ready for database insertion
+//  * @param {array} projectTable ready for database insertion
  */
-export default async function makeTable(toolTable, projectTable) {
-  // WIP modifying github dependencies scrapper to not be output all dependencies
-  // in a raw array, but rather have an object where each object has the
-  // it's dependencies.
-  
-  // const table = [];
+export default async function makeTable(toolTable/*, projectTable*/) {
+  const table = [];
 
-  // const scrapperOfDependencies = new GithubDependenciesScrapper(toolTable);
+  // @todo Diferentiate tool from project
 
-  // return table;
+  // For now I just check if the dependency is in the tool table.
+  // if it's not then nothing.
+
+  const scrapperOfDependencies = new GithubDependenciesScrapper(toolTable);
+  scrapperOfDependencies.setOutputLength(20);
+  scrapperOfDependencies.setMaxAmountOfPageSurfs(20);
+  const dependenciesPerTool = await scrapperOfDependencies.run();
+
+  for (const toolInfo of toolTable) {
+    for (const dependency of dependenciesPerTool[toolInfo.name]) {
+
+      // Check if dependency is a stored tool
+      for (const toolInfo2 of toolTable) {
+        if (toolInfo.name === dependency) {
+          table.push({
+            project_id: toolInfo.tool_id, // just for now because all projects are tools too for now
+            tool_id: toolInfo2.tool_id,
+          });
+        }
+      }
+    }
+  }
+
+  return table;
 }
   
 // makeTable().then((data) => console.log(inspect(data)));
