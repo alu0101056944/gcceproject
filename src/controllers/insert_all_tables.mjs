9a086f3314901900ccc462d07bdacdbf15840311
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-11-12 19:37:01 +0000

    make project company now produces the (project_id, company_id) partial table. Reformat insert all tables file.
    Added insert project company into insert all tables file.

--- src/controllers/insert_all_tables.mjs
+++ src/controllers/insert_all_tables.mjs
@@ -13,6 +13,8 @@ import makeCommunityTable from './make_community_table.mjs';
 import makeCompanyTable from './make_company_table.mjs';
 import makeProjectTable from './make_project_table.mjs';
 import makeMarketTable from './make_market_table.mjs';
+import makeProjectCompany from './make_project_company_table.mjs';
+
 import { inspect } from 'util';
 
 // export default async function insertAllTables() {
@@ -27,31 +29,85 @@ export default async function insertAllTables() {
   await db.none('DELETE FROM $1:raw', ['company']);
   const employeeTable = await makeEmployeeTable();
   const insertEmployeeTable =
-      pgp.helpers.insert(employeeTable,
-        ['employee_id', 'name', 'title', 'department'], 'employee');
+      pgp.helpers.insert(
+          employeeTable,
+          [
+            'employee_id',
+            'name',
+            'title',
+            'department'
+          ],
+          'employee'
+        );
 
-  // const toolTable = await makeToolTable();
-  // const insertToolTable =
-  //     pgp.helpers.insert(toolTable,
-  //       ['tool_id', 'name', 'author_company', 'type', 'specialization'], 'tool');
+  const toolTable = await makeToolTable();
+  const insertToolTable =
+      pgp.helpers.insert(
+          toolTable,
+          [
+            'tool_id',
+            'name',
+            'author_company',
+            'type',
+            'specialization'
+          ],
+          'tool'
+        );
 
-  // const communityTable = await makeCommunityTable();
-  // const insertCommunityTable =
-  //     pgp.helpers.insert(communityTable,
-  //       ['community_id', 'name', 'type'], 'community');
+  const communityTable = await makeCommunityTable();
+  const insertCommunityTable =
+      pgp.helpers.insert(
+          communityTable,
+          [
+            'community_id',
+            'name',
+            'type'
+          ],
+          'community'
+        );
 
   const companyTable = await makeCompanyTable();
-  console.log(inspect(companyTable));
+  // console.log(inspect(companyTable));
   const insertCompanyTable =
-      pgp.helpers.insert(companyTable,
-        ['company_id', 'name', 'employee_amount', 'amount_of_searches', 'type'],
-            'company');
+      pgp.helpers.insert(
+          companyTable,
+          [
+            'company_id',
+            'name',
+            'employee_amount',
+            'amount_of_searches',
+            'type'
+          ],
+          'company'
+        );
 
-  // const projectTable = await makeProjectTable();
-  // const insertProjectTable =
-  //     pgp.helpers.insert(projectTable,
-  //       ['project_id', 'project_name', 'downloads', 'contributors', 'searches'],
-  //           'project');
+  const projectTable = await makeProjectTable();
+  const insertProjectTable =
+      pgp.helpers.insert(
+          projectTable,
+          [
+            'project_id',
+            'project_name',
+            'downloads',
+            'contributors',
+            'searches'
+          ],
+          'project'
+        );
+
+  const projectCompanyTable =
+      await makeProjectCompany(companyTable, projectTable);
+  const insertProjectCompanyTable =
+      pgp.helpers.insert(
+          projectTable,
+          [
+            'project_id',
+            'company_id',
+            'budget',
+            'amount_of_employees_assigned'
+          ],
+          'project_company'
+        );
 
   // const marketTable = await makeMarketTable();
   // const insertMarketTable =
--- src/controllers/make_project_company_table.mjs
+++ src/controllers/make_project_company_table.mjs
@@ -11,7 +11,7 @@ import makeToolsFromGithubExplore from './scrapper_usages/make_tools_from_github
 import { inspect } from 'util';
 
 // assumes companyTable names are github compatible names (without spaces)
-export default async function makeProjectCompany(companyTable, projectTable) {
+export default async function makeProjectCompanyTable(companyTable, projectTable) {
   const specializations = [
     'frontend',
     // 'backend',
@@ -19,17 +19,49 @@ export default async function makeProjectCompany(companyTable, projectTable) {
     // 'devops',
   ];
   const { urlsObject } = (await makeToolsFromGithubExplore(specializations));
-  
-  const nameAuthorPairs = [];
+
+  const names = new Set();
+  const author_companies = new Set();
   for (const url of Object.getOwnPropertyNames(urlsObject)) {
-    const [ _, author_company, name ] = /github.com\/(.*?)\/(.*?)\//.exec(url);
-    nameAuthorPairs.push({ author_company, name });
+    const [ _, AUTHOR_COMPANY, NAME ] = /github.com\/(.*?)\/(.*?)\//.exec(url);
+    names.add(NAME);
+    author_companies.add(AUTHOR_COMPANY);
   }
 
-  for (const pairObject of nameAuthorPairs) {
-
-    const project_id = url
-
+  const authorCompanyIds = {};
+  for (const companyObject of companyTable) {
+    if (author_companies.has(companyObject.name)) {
+      authorCompanyIds[companyObject.author_company] = companyObject.company_id;
+    } else {
+      authorCompanyIds[companyObject.name] = null;
+    }
   }
+
+  const namesIds = {};
+  for (const projectInfo of projectTable) {
+    if (names.has(projectInfo.project_name)) {
+      namesIds[projectInfo.project_name] = projectInfo.project_id;
+    } else {
+      namesIds[projectInfo.project_name] = null;
+    }
+  }
+
+  const projectCompanyTable = [];
+  if (names.length != author_companies.length) {
+    throw new Error('Error when extracting author/name repo from url;' +
+      'different lengths')
+  }
+  const allAuthorCompanyIds = Array.from(authorCompanyIds);
+  names.forEach((name, i) => {
+        projectCompanyTable.push({
+              project_id: namesIds[name],
+              company_id: allAuthorCompanyIds[i],
+            });
+      });
+
+  // needs budget
+
+  // needs amount of employees assigned.
+
+  return projectCompanyTable;
 };
-
