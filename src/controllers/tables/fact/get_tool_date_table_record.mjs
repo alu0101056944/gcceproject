d9cb1a7709290a7de2df7be7472ad960f7c41f82
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-01-01 22:28:46 +0000

    Implemented getVersionChange function at get tool date record

--- src/controllers/tables/fact/get_tool_date_table_record.mjs
+++ src/controllers/tables/fact/get_tool_date_table_record.mjs
@@ -19,12 +19,64 @@
 
 import getInfo from '../../scraper_use_cases/get_repository_info.mjs';
 
+import { compare } from 'compare-versions';
+
+function toChangeType(allVersion) {
+  const checkSemantic = (version) => version.match(/(\d+?)\.(\d+?)\.(\d+?)/);
+  const allSemantic = allVersion.filter(checkSemantic);
+  if (allSemantic.length === 0) {
+    return 'non-semantic';
+  }
+
+  // check if all versions are the same
+  for (let i = 1; i < allSemantic.length; i++) {
+    if (i >= allSemantic.length - 1) {
+      return 'same';
+    }
+    if (!compare(allSemantic[0], allSemantic[i], '=')) {
+      break;
+    }
+  }
+
+  const allNumbers = [];
+  for (const version of allSemantic) {
+    let currentNumber = '';
+    const numbers = [];
+    for (let i = 0; i < version.length; i++) {
+      if (version[i].match(/\d/)) {
+        currentNumber += version[i];
+      } else {
+        numbers.push(parseInt(currentNumber));
+        currentNumber = '';
+      }
+    }
+    allNumbers.push(numbers);
+  }
+
+  let changeType = 'major';
+
+  const allChangeType = ['major', 'minor', 'path'];
+  for (let i = 0; i < 3; i++) { // [major|minor|patch]
+    changeType = allChangeType[i];
+    const relevantNumbers = allNumbers.map(numbers => numbers[i]);
+    const thereIsChange = new Set(relevantNumbers).length > 1;
+    if (thereIsChange) {
+      break;
+    }
+  }
+
+  return changeType;
+}
+
 export default async function getToolDateRecord(toolTable, idOfToday) {
   const allPartialURL =
       toolTable.map(tool => `${tool.author_company}/${tool.name}`);
   const allRepoInfo = await getInfo(allPartialURL);
 
   for (const record of toolTable) {
+    const PARTIAL_URL = `${record.author_company}/${record.name}`;
+    const scraper = new latestFiveVersionsScraper([PARTIAL_URL]);
+    const latestVersions = (await scraper.run())[PARTIAL_URL];
     const record = {
       version: allRepoInfo.version,
       
