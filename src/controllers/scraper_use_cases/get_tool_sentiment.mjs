0ea4cc1b8158b7bf3149810fc6bf4496332e02cf
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-01-04 11:45:50 +0000

    Implemented get tool sentiment scraper use case.

--- src/controllers/scraper_use_cases/get_tool_sentiment.mjs
+++ src/controllers/scraper_use_cases/get_tool_sentiment.mjs
@@ -7,6 +7,15 @@
 
 'use strict';
 
-export default async function countDiscussionAmount() {
-  
+import RedditPostsScrapper from '../../routes/scrapers/reddit-posts-scraper.mjs';
+
+export default async function getAllToolSentiment(toolTable) {
+  const allTotalSentiment = [];
+  for (const toolRecord of toolTable) {
+    const toURL = (toolName) => `https://www.reddit.com/r/programming/search/?q=${toolName}&restrict_sr=1&sort=new`;
+    const scraper = new RedditPostsScrapper(toURL(toolRecord.name));
+    const allSentiment = await scraper.run();
+    allTotalSentiment.push(allSentiment.reduce((acc, sent) => acc + sent, 0));
+  }
+  return allTotalSentiment;
 }
