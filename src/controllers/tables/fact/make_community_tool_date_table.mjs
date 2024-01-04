6abe4d6dfc503f3d13513797b399d2aca4038ca8
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-01-04 12:26:39 +0000

    Finished make community-tool-date table.

--- src/controllers/tables/fact/make_community_tool_date_table.mjs
+++ src/controllers/tables/fact/make_community_tool_date_table.mjs
@@ -7,21 +7,26 @@
 
 'use strict';
 
+import getAllToolSentiment from "../../scraper_use_cases/get_tool_sentiment.mjs";
+
 // tool_score
 // rank = NULL due to not having a rank at hand *** UPDATE README.MD REQUIRED ***
 async function getAllToolInfoFromGithub(toolTable, idOfToday) {
   const allToolInfo = [];
 
-  const allPartialURL =
-      toolTable.map(tool => `${tool.author_company}/${tool.name}`);
+  const allTotalSentiment = await getAllToolSentiment(toolTable);
+
+  if (allTotalSentiment.length !== toolTable.length) {
+    throw new Error('Detected difference between allSentimen length and ' +
+        'tool table length, something is wrong.');
+  }
 
   for (const [index, toolRecord] of toolTable.entries()) {
-    const PARTIAL_URL = `${toolRecord.author_company}/${toolRecord.name}`;
     const record = {
       community_id: 1, // github's id in the make community table file
       tool_id: toolRecord.tool_id,
       date_id: idOfToday,
-      // tool_score
+      tool_score: allTotalSentiment[index],
       rank: null
     }
     allToolInfo.push(record);
@@ -45,7 +50,7 @@ export default async function makeCommunityToolDateTable(communityTable,
   for (const community of communityTable) {
     const allInfoAtCommunity =
         await nameToAllInfo[community.name](toolTable, idOfToday);
-    allInfoAtCommunity.concat(allInfoAtCommunity);
+    allInfoAtCommunity.forEach(record => allRecord.push(record));
   }
 
   return allRecord;
